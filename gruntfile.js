c4dff6fa737550ca501b72f7ea0b42c7200ecad9
Peter Eriksson <that.peter@gmail.com>
2025-12-11 16:54:24 +0100

    refactor: remove deprecated documentation files and scripts
    - Deleted old HTML documentation files: index.html and redirector pages.
    - Removed YUIDoc related scripts and deployment files as YUIDoc is deprecated.
    - Updated build process to use Uglify instead of Google Closure Compiler for minification.
    - Changed package name to scoped package @stinky/litegraph.js.
    - Cleaned up utility scripts related to building and testing.
    - Added GitHub Actions workflow for automated npm publishing on release.

--- gruntfile.js
+++ gruntfile.js
@@ -21,27 +21,22 @@ module.exports = function (grunt) {
         dest: 'build/litegraph.js'
       }
     },
-    closureCompiler: {
-
+    uglify: {
       options: {
-        compilerFile: 'node_modules/google-closure-compiler/compiler.jar',
-        compilerOpts: {
-          formatting: 'pretty_print',
-          warning_level: 'default'
-        },
-        d32: false, // will use 'java -client -d32 -jar compiler.jar'
-        TieredCompilation: false// will use 'java -server -XX:+TieredCompilation -jar compiler.jar',
-        // ,output_wrapper: '"var LiteGraph = (function(){%output% return LiteGraph;}).call(this);"'      //* Make container for all
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> */\n',
+        compress: {
+          drop_console: false
+        }
       },
-      targetName: {
-        src: '<%= projectFiles %>',
+      build: {
+        src: 'build/litegraph.js',
         dest: 'build/litegraph.min.js'
       }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-concat')
-  grunt.loadNpmTasks('grunt-closure-tools')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
 
-  grunt.registerTask('build', ['concat:build', 'closureCompiler'])
+  grunt.registerTask('build', ['concat:build', 'uglify:build'])
 }
